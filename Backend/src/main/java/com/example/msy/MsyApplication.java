2f644fb54510b0075168ce3acdf9c34aac2cde71
YoussefHussien99 <51840033+youssefa111@users.noreply.github.com>
2023-02-08 13:38:54 +0200

    story-1
    Start Backend env

---
+++ Backend/src/main/java/com/example/msy/MsyApplication.java
@@ -0,0 +1,13 @@
+package com.example.msy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsyApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/msy/MsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
